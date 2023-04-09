d1685b8eeb13b5f2999f7d38e6248a1d01563023
Arya Sharma <arya19@live.unc.edu>
2023-04-09 12:52:15 -0400

    "Testing if outputs work for no input moves"

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.get("*", (req, res) => {
     if (app_endpoint.test(path)) {
         res.status(200).send("200 OK");
     } else if (rps_endpoint.test(path)) {
-
+        res.send(JSON.stringify(rps()));
     } else if (rpsls_endpoint.test(path)) {
-
+        res.send(JSON.stringify(rpsls()));
     } else if (rps_play_json_endpoint.test(path)) {
 
     } else if (rps_play_url_endpoint.test(path)) {
