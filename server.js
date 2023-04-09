4a3e61f4290a2337b13fe8b45acde1fab22d1549
Arya Sharma <arya19@live.unc.edu>
2023-04-08 23:12:02 -0400

    "downloaded express, set up err 404"

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
-import { rps } from "/lib/rpsls.js";
-import { rpsls } from "/lib/rpsls.js";
+import { rps } from "./lib/rpsls.js";
+import { rpsls } from "./lib/rpsls.js";
 import minimist from "minimist";
+import express from "express";
+
+const args = minimist(process.argv.slice(2));
+const port = args.port || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(404).send("NOT FOUND");
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
+
+
+
