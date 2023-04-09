2ee4d530329cc49c5f485727f4b880f40b74718c
Arya Sharma <arya19@live.unc.edu>
2023-04-09 13:36:52 -0400

    Added body parser and post capability

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import { rps } from "./lib/rpsls.js";
 import { rpsls } from "./lib/rpsls.js";
 import minimist from "minimist";
 import express from "express";
+import bodyParser from "body-parser";
 
 const args = minimist(process.argv.slice(2));
 const port = args.port || 5000;
@@ -18,27 +19,49 @@ const rpsls_play_only_endpoint = /^\/app\/rpsls\/play\/((r|R)ock|(p|P)aper|(s|S)
 
 const app = express();
 
+
+app.post("/app/rps/play/", (req, res) => {
+    console.log(req.path);
+    var path = req.path;
+    
+    if (rps_play_json_endpoint.test(path)) {
+        path = path.replace("/app/rps/play/", "");
+        path = JSON.parse(path);
+        res.send(JSON.stringify(rps(path.shot)));
+    
+    } else if (rps_play_url_endpoint.test(path)) {
+
+    
+    } else if (rpsls_play_json_endpoint.test(path)) {
+
+    
+    } else if (rpsls_play_url_endpoint.test(path)) {
+
+    
+    } else if (rps_play_only_endpoint.test(path)) {
+
+    
+    } else if (rpsls_play_only_endpoint.test(path)) {
+
+    } else {
+        res.status(400).send("404 NOT FOUND");
+    }
+});
+
+
+
 app.get("*", (req, res) => {
     var path = req.path;
     if (app_endpoint.test(path)) {
         res.status(200).send("200 OK");
+    
     } else if (rps_endpoint.test(path)) {
         res.send(JSON.stringify(rps()));
+    
     } else if (rpsls_endpoint.test(path)) {
         res.send(JSON.stringify(rpsls()));
-    } else if (rps_play_json_endpoint.test(path)) {
-
-    } else if (rps_play_url_endpoint.test(path)) {
-
-    } else if (rpsls_play_json_endpoint.test(path)) {
-
-    } else if (rpsls_play_url_endpoint.test(path)) {
-
-    } else if (rps_play_only_endpoint.test(path)) {
-
-    } else if (rpsls_play_only_endpoint.test(path)) {
-
-    } else {
+    } 
+    else {
         res.status(400).send("404 NOT FOUND");
     }
 });
